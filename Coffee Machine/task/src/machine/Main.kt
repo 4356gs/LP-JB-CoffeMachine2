ed614b5439881802abb7cc18155c7b1ae33a64c3
Gelo Saldana <geloalex@gmail.com>
2022-10-31 09:20:00 -0400

    step 4/6

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,25 +1,86 @@
 package machine
 
 fun main() {
-    println(
-        "Starting to make a coffee\n" +
-        "Grinding coffee beans\n" +
-        "Boiling water\n" +
-        "Mixing boiled water with crushed coffee beans\n" +
-        "Pouring coffee into the cup\n" +
-        "Pouring some milk into the cup\n" +
-        "Coffee is ready!"
-        
-    )
-    println("Write how many cups of coffee you will need:")
-    val cupsNum = readln().toInt()
-    println("For $cupsNum cups of coffee you will need:")
-    val water = 200
-    val milk = 50
-    val coffee = 15
+    var money = 550
+    var water = 400
+    var milk = 540
+    var coffee = 120
+    var disposable = 9
+    printMenu(water, milk, coffee, disposable, money)
+    println("Write acction (buy, fill, take):")
+    val action = readln()
+    when (action) {
+        "buy" -> {buy(water, milk, coffee, disposable, money)}
+        "fill" -> {fill(water, milk, coffee, disposable, money)}
+        "take" -> {take(water, milk, coffee, disposable, money)}
+    }
 
-    println("${water * cupsNum} ml of water")
-    println("${milk * cupsNum} ml of milk")
-    println("${coffee * cupsNum} g of coffee beans")
 }
+
+fun buy(water: Int,milk: Int,coffee: Int, disposable: Int, money: Int ) {
+    println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
+    val op = readln().toInt()
+    var water = water
+    var milk = milk
+    var coffee = coffee
+    var disposable = disposable
+    var money = money
+    when (op) {
+        1 ->{
+            water -= 250
+            coffee -= 16
+            disposable -= 1
+            money += 4
+
+        }
+        2 ->{
+            water -= 350
+            milk -= 75
+            coffee -= 20
+            disposable -= 1
+            money += 7
+
+        }
+        3 ->{
+            water -= 200
+            milk -= 100
+            coffee -= 12
+            disposable -= 1
+            money += 6
+
+        }
+    }
+    printMenu(water, milk, coffee, disposable, money)
+}
+fun fill(water: Int,milk: Int,coffee: Int, disposable: Int, money: Int ) {
+    println("Write how many ml of water you want to add: ")
+    var water = water + readln().toInt()
+    println("Write how many ml of milk you want to add: ")
+    var milk = milk + readln().toInt()
+    println("Write how many grams of coffee beans you want to add: ")
+    var coffee = coffee + readln().toInt()
+    println("Write how many disposable cups you want to add: ")
+    var disposable = disposable + readln().toInt()
+
+    printMenu(water, milk, coffee, disposable, money)
+}
+fun take(water: Int,milk: Int,coffee: Int, disposable: Int, money: Int ) {
+    var water = water
+    var milk = milk
+    var coffee = coffee
+    var disposable = disposable
+    println("I gave you $money")
+    println()
+    var money = 0
+    printMenu(water, milk, coffee, disposable, money)
+}
+fun printMenu(water: Int,milk: Int,coffee: Int, disposable: Int, money: Int ) {
+    println("The coffe machine has:\n" +
+            "$water ml of water\n" +
+            "$milk ml of milk\n" +
+            "$coffee ml of coffee beans\n" +
+            "$disposable disponsable cups\n" +
+            "$$money of money")
+}
+
 
